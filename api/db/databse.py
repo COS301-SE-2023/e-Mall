b736edf649d87bebfafa0d840ebf871cd6dfd994
J Yoon <u17260044@tuks.co.za>
2023-05-20 12:25:54 +0200

    removed db variables for merge

--- api/db/databse.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-engine = psycopg2.connect(
-    database="postgres",
-    user="my_user_name",
-    password="abc123def345",
-    host="my-rds-table-name.123456.us-east-1.rds.amazonaws.com",
-    port='5432'
-)
