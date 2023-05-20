8be3da91b4dee7aa5d5f2bf4263549ecacf9578e
Kaityss <kaitlynsookdhew@gmail.com>
2023-05-20 23:22:57 +0200

    Added Pending
    Added ui to pending must fix display issues

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { PendingComponent } from './pending/pending.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent },
+  { path: '', redirectTo: '/pending', pathMatch: 'full' },
+  { path: 'pending', component: PendingComponent },
 ];
 
 @NgModule({
