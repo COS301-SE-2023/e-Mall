fd540c2bf0db6ec5174aec6955964cd53b296351
Kaityss <kaitlynsookdhew@gmail.com>
2023-05-20 22:02:53 +0200

    Sign up added
    added ui for signup. there are no pictures. and functionality needs to be added

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: SignInComponent },
+  { path: '', redirectTo: '/sign-up', pathMatch: 'full' },
+  { path: 'sign-up', component: SignUpComponent },
 ];
 
 @NgModule({
