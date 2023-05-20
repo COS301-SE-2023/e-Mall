ab6684a7137fabb533d093b546776112865a9c0d
Kaityss <kaitlynsookdhew@gmail.com>
2023-05-20 21:43:02 +0200

    Added sign-up ui
    Added the sign up ui according to figma but there are still issues also note i do not have pictures

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: SignInComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
