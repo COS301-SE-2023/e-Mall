f52e61e2080c9e2104cf479c7d3ed59aab22f467
Kaityss <kaitlynsookdhew@gmail.com>
2023-05-21 23:49:56 +0200

    added logo
    Added logo to sign in and sign up

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
