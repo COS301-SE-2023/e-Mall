56e935638c3772d867a618959fb226852227fa7b
Kaitlyn Sookdhew <89077850+Kaityss@users.noreply.github.com>
2023-05-22 15:55:33 +0200

    Revert "fixed buttons"

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
