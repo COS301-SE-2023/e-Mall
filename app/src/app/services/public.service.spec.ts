edbf405af96217e937fadbfce8d92cbcf13e8027
munashemash <u19077450@tuks.co.za>
2023-05-23 10:23:47 +0200

    Another unit test passed

--- app/src/app/services/public.service.spec.ts
+++ app/src/app/services/public.service.spec.ts
@@ -1,5 +1,6 @@
 import 'zone.js';
 import 'zone.js/testing';
+import "@angular/compiler";
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
