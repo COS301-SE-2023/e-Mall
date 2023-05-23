93d27e961052cceb0335a5462fa9c79433382022
J Yoon <u17260044@tuks.co.za>
2023-05-23 08:18:53 +0200

    some api and test for user register and get users

--- api/staffs/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Staffs
-
-
-class StaffsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Staffs
-        fields = (
-            'id', 'fname', 'sname', 'password', 'email', 'role', 'created_at', 'last_login', 'is_staff', 'is_active', 'is_superuser')
