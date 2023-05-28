05595c22ead0664f6d321b65249839e2d6e27cde
Gerard Nagura <u19004232@tuks.co.za>
2023-05-28 09:37:48 +0200

    Delete app/src/app directory

--- app/src/app/construction/construction.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-construction',
-  templateUrl: './construction.component.html',
-  styleUrls: ['./construction.component.scss'],
-})
-export class ConstructionComponent {}
--- app/src/app/pending/pending.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pending',
-  templateUrl: './pending.component.html',
-  styleUrls: ['./pending.component.scss'],
-})
-export class PendingComponent {}
