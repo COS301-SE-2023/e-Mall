81df0d671d31aa96c4194890dcab7b2f1402c062
Letlhogonolo Rakgantsho <u21612812@tuks.co.za>
2023-06-07 00:52:11 +0200

    added input validation to sign up page

--- app/src/app/sign-up/sign-up.component.ts
+++ app/src/app/sign-up/sign-up.component.ts
@@ -1,23 +1,65 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 import { IUser, CognitoService } from '@app/services/cognito.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {
+export class SignUpComponent { 
   loading: boolean;
   isConfirm: boolean;
   user: IUser;
+  signUpForm!: FormGroup;
+  formSubmitted = false;
 
-  constructor(private router: Router, private cognitoService: CognitoService) {
+  constructor(private router: Router, private cognitoService: CognitoService,  private formBuilder: FormBuilder) {
     this.loading = false;
     this.isConfirm = false;
     this.user = {} as IUser;
   }
+
+  ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      cpassword: ['', Validators.required],
+    }, {
+      validators: this.passwordMatchValidator
+    });
+  }
+
+  get f() { return this.signUpForm.controls; }
+
+  onSubmit() {
+
+    // Handle form submission
+    if (this.signUpForm.valid) {
+
+      console.log('Form submitted');
+      this.router.navigate(['/']);
+    } else {
+
+      console.log('Form is invalid!');
+      this.formSubmitted = true;
+    }
+  }
+
+   //check if password and confim password match
+   passwordMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const cpassword = formGroup.get('cpassword')?.value;
+  
+    if (password !== cpassword) {
+      formGroup.get('cpassword')?.setErrors({ passwordMismatch: true });
+    } else {
+      formGroup.get('cpassword')?.setErrors(null);
+    }
+  }
+
 
   public signUp(): void {
     /*  this.loading = true;
