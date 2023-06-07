3d063f7cee3161db2844e2e43fbc823f615ca963
J Yoon <u17260044@tuks.co.za>
2023-06-07 18:39:39 +0200

    branch fix 71

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+
+  search() {
+    this.router.navigate(['/construction']);
+  }
+}
