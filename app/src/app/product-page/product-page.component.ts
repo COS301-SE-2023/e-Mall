de6ad64bcfccab1b3fb7df74c354a1be01c5231b
Letlhogonolo Rakgantsho <u21612812@tuks.co.za>
2023-06-08 06:34:44 +0200

    Added Navbar

---
+++ app/src/app/product-page/product-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.scss']
+})
+export class ProductPageComponent {
+
+}
