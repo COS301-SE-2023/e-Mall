0723d5cc3f91384b028026e98130df8280a5f04a
Kaityss <kaitlynsookdhew@gmail.com>
2023-06-12 03:03:31 +0200

    Added products carousel

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
-
+import { HttpClient } from '@angular/common/http';
+interface Product {
+  image: string;
+  title: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+  products: Product[] | undefined;
+  constructor(private router: Router, private http: HttpClient) {}
 
+  ngOnInit() {
+    this.fetchProducts();
+  }
 
-
+  fetchProducts() {
+    this.http.get<Product[]>('/api/products/frontend').subscribe(
+      response => {
+        this.products = response;
+      },
+      error => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
 
   search() {
     this.router.navigate(['/construction']);
