c04599aa3ea360e1951371f541a8b4d32cb49a91
Letlhogonolo Rakgantsho <u21612812@tuks.co.za>
2023-06-15 05:17:24 +0200

    fetch products from api

--- app/src/app/product-page/product-page.component.ts
+++ app/src/app/product-page/product-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductPageService } from '@service/product-page.service';
+
+
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss']
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit{
+
+  products: any;
+
+  constructor(private productService: ProductPageService) {}
+
+  ngOnInit() {
+    this.productService.getProductData().subscribe(
+      (response) => {
+        this.products = response;
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
 }
