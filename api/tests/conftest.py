74dabf78f0e6b6c3935c092658fdedb75c39ccc7
Gerard Nagura <u19004232@tuks.co.za>
2023-06-16 21:51:48 +0200

    Added productseller unit tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -2,7 +2,9 @@ from pytest_factoryboy import register
 from .factories import ConsumerFactory
 from .factories import SellerFactory
 from .factories import ProductFactory
+from .factories import ProductSellerFactory
 
 register(ConsumerFactory)
 register(SellerFactory)
 register(ProductFactory)
+register(ProductSellerFactory)
