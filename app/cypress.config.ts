7a57be2faf02e78ec88965dcbbc7746018b1d473
J Yoon <u17260044@tuks.co.za>
2023-06-25 18:54:53 +0200

    cypress

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -1,18 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from 'cypress';
-
+import mergeReports from 'cypress-sonarqube-reporter/mergeReports';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      on('after:run', results => {
+        // /!\ don't forget to return the Promise /!\
+        return mergeReports(results, {
+          mergeFileName: 'coverage.xml',
+          mergeOutputDir: 'coverage/cypress',
+        });
+      });
+      return config;
     },
     baseUrl: 'http://localhost:4200',
-    reporter: 'mocha-junit-reporter',
-    // video: false,
+    reporter: 'cypress-sonarqube-reporter',
     reporterOptions: {
-      mochaFile: 'coverage/cypress/coverage.xml',
-      // video: false,
-      // quiet: true,
+      // outputDir: './coverage',
+      overwrite: true,
+      // mergeFileName: 'coverage.xml',
       // see "Reporter Options" section
     },
   },
