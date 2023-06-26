b339f6673f7b6129ceb2cb4835657eb8b2ac7f94
Kaityss <kaitlynsookdhew@gmail.com>
2023-06-26 12:28:24 +0200

    added unit tests

--- app/src/app/features/not-found/not-found.component.spec.ts
+++ app/src/app/features/not-found/not-found.component.spec.ts
@@ -45,4 +45,55 @@ describe('NotFoundComponent', () => {
       state: undefined
     });
   });
+  
+  it('should display "404" as the heading', () => {
+    // Create a div element with the specified classes
+    const div = document.createElement('div');
+    div.id = 'notfound';
+    div.className = 'notfound';
+
+    // Create a div element for the 404 heading
+    const notfound404Div = document.createElement('div');
+    notfound404Div.className = 'notfound-404';
+
+    // Create an h1 element for the 404 text
+    const heading = document.createElement('h1');
+    heading.textContent = '404';
+
+    // Append the heading to the notfound-404 div
+    notfound404Div.appendChild(heading);
+
+    // Create an h2 element for the error message
+    const errorMessage = document.createElement('h2');
+    errorMessage.textContent = 'We are sorry, Page not found!';
+
+    // Create a p element for the error description
+    const errorDescription = document.createElement('p');
+    errorDescription.textContent =
+      'The page you are looking for might have been removed had its name changed or is temporarily unavailable.';
+
+    // Create an anchor element for the back to homepage link
+    const backToHomepageLink = document.createElement('a');
+    backToHomepageLink.href = '/home';
+    backToHomepageLink.textContent = 'Back To Homepage';
+
+    // Append the elements to build the desired HTML structure
+    div.appendChild(notfound404Div);
+    div.appendChild(errorMessage);
+    div.appendChild(errorDescription);
+    div.appendChild(backToHomepageLink);
+
+    // Append the main div to the body of the document
+    document.body.appendChild(div);
+
+    // Find the heading element in the document
+    const headingElement = document.querySelector('.notfound-404 h1') as HTMLAnchorElement;
+
+    // Assert that the heading text is "404"
+    expect(headingElement.textContent).toEqual('404') ;
+  });
+
+  
+
+   
 });
