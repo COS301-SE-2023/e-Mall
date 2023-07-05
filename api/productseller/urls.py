22412671eb2e95c2e373ab703c06d8b7aec4ea23
Gerard Nagura <u19004232@tuks.co.za>
2023-07-05 14:03:36 +0200

    productseller update function

--- api/productseller/urls.py
+++ api/productseller/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductSellerFrontendAPIView, ProductSellerBackendAPIView
+from .views import ProductSellerFrontendAPIView, ProductSellerBackendAPIView, ProductSellerProdUpdateAPIView
 
 urlpatterns = [
     path("frontend/", ProductSellerFrontendAPIView.as_view()),
     path("backend/", ProductSellerBackendAPIView.as_view()),
+    path("produpdate/", ProductSellerProdUpdateAPIView.as_view())
 ]
--- api/productseller/views.py
+++ api/productseller/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import ProductSeller
 from .serializers import ProductSellerSerializer
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.permissions import AllowAny
 
 
 # Create your views here.
@@ -18,14 +22,13 @@ class ProductSellerFrontendAPIView(APIView):
 class ProductSellerBackendAPIView(APIView):
     def get(self, request):
         # Input for search of a specific product
-        prod_id = int(request.GET.get("prod_id")
-                      ) if request.GET.get("prod_id") else 1
+        prod_id = int(request.GET.get("prod_id")) if request.GET.get("prod_id") else 1
 
         # input for sort[price, discount]
         sort = request.GET.get("sort")
         filter_in_stock = request.GET.get("filter_in_stock")
 
-        if (sort is None):
+        if sort is None:
             sort = "price"
         productseller = ProductSeller.objects.all()
 
@@ -40,3 +43,42 @@ class ProductSellerBackendAPIView(APIView):
 
         serializer = ProductSellerSerializer(productseller, many=True)
         return Response(serializer.data)
+
+
+class ProductSellerProdUpdateAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        prod_id = request.data.get("prod_id")
+        seller_name = request.data.get("seller_name")
+        try:
+            # Fetch the ProductSeller object based on prod_id and seller_name
+            productseller = ProductSeller.objects.get(
+                seller__business_name=seller_name, product__id=prod_id
+            )
+
+            # Update the fields of the ProductSeller object
+            productseller.original_price = request.data.get(
+                "original_price", productseller.original_price
+            )
+            productseller.price = request.data.get("price", productseller.price)
+            productseller.discount = request.data.get(
+                "discount", productseller.discount
+            )
+            productseller.discount_rate = request.data.get(
+                "discount_rate", productseller.discount_rate
+            )
+            productseller.in_stock = request.data.get(
+                "in_stock", productseller.in_stock
+            )
+
+            # Save the updated ProductSeller object
+            productseller.save()
+
+            # Return a success response
+            return JsonResponse(
+                {"message": "ProductSeller details updated successfully"}
+            )
+
+        except ProductSeller.DoesNotExist:
+            return JsonResponse({"error": "ProductSeller not found"}, status=404)
