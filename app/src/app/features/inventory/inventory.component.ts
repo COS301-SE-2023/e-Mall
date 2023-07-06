5f3c1c58968aab1cb00e36c837901e157c919dba
munashemash <u19077450@tuks.co.za>
2023-07-06 18:48:36 +0200

    comp + module set up

---
+++ app/src/app/features/inventory/inventory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.scss']
+})
+export class InventoryComponent {
+  // add logic here
+}
