f1cd738dbfc7628ce2f57a2c68ea69bed5732e5a
Kaityss <kaitlynsookdhew@gmail.com>
2023-07-07 17:13:51 +0200

    Added functionality to click

--- app/src/app/shared/components/seller-nav/seller-nav.component.ts
+++ app/src/app/shared/components/seller-nav/seller-nav.component.ts
@@ -1,10 +1,17 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Component } from '@angular/core';
 
+import { Router, NavigationExtras } from '@angular/router';
+
 @Component({
   selector: 'app-seller-nav',
   templateUrl: './seller-nav.component.html',
   styleUrls: ['./seller-nav.component.scss'],
 })
 export class SellerNavComponent {
+  constructor(private router: Router) {
+  }
+navigateTo(page: string): void {
+  this.router.navigate([`/${page}`]);
+}
 }
