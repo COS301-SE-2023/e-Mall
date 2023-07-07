654c3865bfd6e15d9824ee318c46f0a32c68d626
Kaityss <kaitlynsookdhew@gmail.com>
2023-07-07 16:38:54 +0200

    added side nav bar for seller dashboard

---
+++ app/src/app/shared/components/seller-nav/seller-nav.component.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-nav',
+  templateUrl: './seller-nav.component.html',
+  styleUrls: ['./seller-nav.component.scss'],
+})
+export class SellerNavComponent {
+}
