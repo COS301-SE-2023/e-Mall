85a95f583313ca6460317ea0f793822a8ce265b1
Kaityss <kaitlynsookdhew@gmail.com>
2023-07-07 16:39:19 +0200

    updated sales page with new nav bar

--- app/src/app/features/sales/sales.module.ts
+++ app/src/app/features/sales/sales.module.ts
@@ -5,9 +5,11 @@ import { SalesRoutingModule } from './sales-routing.module';
 import { SalesComponent } from './sales.component';
 import { IonicModule } from '@ionic/angular';
 
+import { SellerNavModule } from '@shared/components/seller-nav/seller-nav.module';
+
 @NgModule({
   declarations: [SalesComponent],
-  imports: [CommonModule, SalesRoutingModule, IonicModule],
+  imports: [CommonModule, SalesRoutingModule, IonicModule,SellerNavModule],
   exports: [SalesComponent],
 })
 export class SalesModule {}
