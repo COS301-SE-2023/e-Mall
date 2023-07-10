d403d82edf060735718e080476ba07e67a172c43
munashemash <u19077450@tuks.co.za>
2023-07-10 09:55:36 +0200

    update

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -31,7 +31,7 @@ import { NgxsDispatchPluginModule } from '@ngxs-labs/dispatch-decorator';
 import { AuthModule } from './features/auth/auth.module';
 import { ErrorModule } from './features/error/error.module';
 import { NgxsResetPluginModule } from 'ngxs-reset-plugin';
-import { customPageAnimation } from '@shared/animations/custom.animation';
+import { customPageAnimation } from '@shared/animations/custom-page.animation';
 import { InventoryModule } from './features/inventory/inventory.module';
 import { SellerNavModule } from '@shared/components/seller-nav/seller-nav.module';
 @NgModule({
