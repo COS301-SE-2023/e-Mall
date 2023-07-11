a38c62084ce94ddb3584c24b48c377152b01e1c6
munashemash <u19077450@tuks.co.za>
2023-07-11 11:49:31 +0200

    products routing + popup

---
+++ app/src/app/features/popover/popover.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+@Component({
+    selector: 'app-popover',
+    templateUrl: './popover.component.html',
+    styleUrls: ['./popover.component.scss'],
+})
+
+export class PopoverComponent {
+    @Input() product: any;
+
+    constructor(private popoverController: PopoverController) { }
+
+    editProduct() {
+        // Implement the logic to navigate to the edit product page with the selected product's ID or other identifier
+        this.popoverController.dismiss();
+      }
+    
+      deleteProduct() {
+        // Implement the logic to delete the selected product
+        this.popoverController.dismiss();
+      }
+
+    closePopover() {
+        this.popoverController.dismiss();
+    }
+}
