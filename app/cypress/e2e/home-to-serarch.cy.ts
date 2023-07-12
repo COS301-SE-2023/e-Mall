ac3ded3b271c073d79b384eaafc6e1f097e26b19
Kaityss <kaitlynsookdhew@gmail.com>
2023-07-12 20:10:42 +0200

    updated e2e tests

--- app/cypress/e2e/home-to-serarch.cy.ts
+++ app/cypress/e2e/home-to-serarch.cy.ts
@@ -3,7 +3,7 @@ import 'cypress-network-idle';
 describe('visit home and search', () => {
   it('passes', () => {
     cy.visit('/home');
-   /* cy.get('#mat-input-0').click({ force: true }).type('f').type('{enter}');
+    cy.get('#mat-input-0').click({ force: true }).type('f').type('{enter}');
     cy.url().should('match', /search-results/);
     cy.waitForNetworkIdlePrepare({
       method: 'GET',
@@ -11,43 +11,43 @@ describe('visit home and search', () => {
       alias: 'api',
     });
     cy.waitForNetworkIdle('@api', 1000);
-    cy.get('img[class*="product-image"]').should('have.length', 10);
+    cy.get('app-product-card').should('have.length', 10);
 
-    cy.contains('Brand')
+   cy.contains('Brand')
       .click({ force: true })
       .then(() => {
         cy.contains('ONE').click({ force: true });
       });
-    cy.get('img[class*="product-image"]').should('have.length', 2);
+    cy.get('app-product-card').should('have.length', 2);
 
     cy.contains('ONE').click({ force: true });
-    cy.get('img[class*="product-image"]').should('have.length', 10);
+    cy.get('app-product-card').should('have.length', 10);
 
     cy.contains('Category')
       .click({ force: true })
       .then(() => {
         cy.contains('books').click({ force: true });
       });
-    cy.get('img[class*="product-image"]').should('have.length', 9);
+    cy.get('app-product-card').should('have.length', 9);
     cy.contains('books').click({ force: true });
-    cy.get('img[class*="product-image"]').should('have.length', 10);
+    cy.get('app-product-card').should('have.length', 10);
 
     cy.contains('Inventory')
       .click({ force: true })
       .then(() => {
         cy.contains('Show only in stock').click({ force: true });
       });
-    cy.get('img[class*="product-image"]').should('have.length', 9);
+    cy.get('app-product-card').should('have.length', 9);
     cy.contains('Show only in stock').click({ force: true });
-    cy.get('img[class*="product-image"]').should('have.length', 10);
+    cy.get('app-product-card').should('have.length', 10);
 
     cy.contains('Seller')
       .click({ force: true })
       .then(() => {
-        cy.contains('Amazon').click({ force: true });
+        cy.contains('Smith LLC').click({ force: true });
       });
-    cy.get('img[class*="product-image"]').should('have.length', 7);
-    cy.contains('Amazon').click({ force: true });
-    cy.get('img[class*="product-image"]').should('have.length', 10);*/
+    cy.get('app-product-card').should('have.length', 1);
+    cy.contains('Evans-Knapp').click({ force: true });
+    cy.get('app-product-card').should('have.length', 3);
   });
 });
