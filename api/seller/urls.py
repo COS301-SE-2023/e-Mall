09ccd4980054d233cbe8e2624a148df968d0ffe8
Gerard Nagura <u19004232@tuks.co.za>
2023-07-19 22:50:18 +0200

    seller info and update seller post reqs

--- api/seller/urls.py
+++ api/seller/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='seller_register'),
-    path('auth_test/', views.auth_test, name='auth_test')
+    path("register/", views.register, name="seller_register"),
+    path("auth_test/", views.auth_test, name="auth_test"),
+    path("seller_info/", views.get_seller_info, name="seller_info"),
+    path("update_info/", views.update_seller_info, name="update_info"),
 ]
