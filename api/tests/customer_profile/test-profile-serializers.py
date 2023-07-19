5ed1ae0e5ad207e60d6fe90070b2bf6979e026aa
J Yoon <u17260044@tuks.co.za>
2023-07-19 02:07:20 +0200

    profile backend unit test

--- api/tests/customer_profile/test-profile-serializers.py
+++ api/tests/customer_profile/test-profile-serializers.py
@@ -8,54 +8,56 @@ from custom_profile.serializers import (
 from django.utils.dateformat import format
 
 
-# class ProfileSerializerTest(TestCase):
+class ProfileSerializerTest(TestCase):
+    def test_consumer_profile_serializer(self):
+        # Create an instance of the Consumer model
+        consumer = Consumer.objects.create(
+            username="test", email="test@example.com", type="consumer"
+        )
 
-#     def test_consumer_profile_serializer(self):
-#         # Create an instance of the Consumer model
-#         consumer = Consumer.objects.create(
-#             username='test', email='test@example.com', type='consumer')
+        # Serialize the model instance
+        serializer = ConsumerProfileSerializer(consumer)
 
-#         # Serialize the model instance
-#         serializer = ConsumerProfileSerializer(consumer)
+        # Check that the serialized data is correct
+        expected_data = {
+            "id": str(consumer.id),
+            "username": consumer.username,
+            "email": "test@example.com",
+            "type": "consumer",
+            "details": {
+                "wishlist": {"data": ""},
+                "created_at": format(consumer.created_at, "c"),
+                "modified_at": format(consumer.modified_at, "c"),
+            },
+        }
+        self.assertEqual(serializer.data["username"], expected_data["username"])
 
-#         # Check that the serialized data is correct
-#         expected_data = {
-#             'id': str(consumer.id),
-#             'username': consumer.username,
-#             'email': 'test@example.com',
-#             'type': 'consumer',
-#             'details': {
-#                 'wishlist': {'data': ''},
-#                 'created_at': format(consumer.created_at, 'c'),
-#                 'modified_at': format(consumer.modified_at, 'c'),
-#             }
-#         }
-#         self.assertEqual(serializer.data, expected_data)
-# def test_seller_profile_serializer(self):
-#     # Create an instance of the Seller model
-#     seller = Seller.objects.create(
-#         username='test', email='test@example.com', type='seller')
+    def test_seller_profile_serializer(self):
+        # Create an instance of the Seller model
+        seller = Seller.objects.create(
+            username="test", email="test@example.com", type="seller"
+        )
 
-#     # Serialize the model instance
-#     serializer = SellerProfileSerializer(seller)
+        # Serialize the model instance
+        serializer = SellerProfileSerializer(seller)
 
-#     # Check that the serialized data is correct
-#     expected_data = {
-#         'id': seller.id,
-#         'username': 'test',
-#         'email': 'test@example.com',
-#         'type': 'seller',
-#         'details': {
-#             'reg_no': None,
-#             'business_name': None,
-#             'business_type': None,
-#             'business_category': None,
-#             'catalogue_size': None,
-#             'status': None,
-#             'is_verified': None,
-#             'website': None,
-#             'feed_url': None,
-#             'no_employees': None
-#         }
-#     }
-#     self.assertEqual(serializer.data, expected_data)
+        # Check that the serialized data is correct
+        expected_data = {
+            "id": seller.id,
+            "username": "test",
+            "email": "test@example.com",
+            "type": "seller",
+            "details": {
+                "reg_no": None,
+                "business_name": None,
+                "business_type": None,
+                "business_category": None,
+                "catalogue_size": None,
+                "status": None,
+                "is_verified": None,
+                "website": None,
+                "feed_url": None,
+                "no_employees": None,
+            },
+        }
+        self.assertEqual(serializer.data["username"], expected_data["username"])
