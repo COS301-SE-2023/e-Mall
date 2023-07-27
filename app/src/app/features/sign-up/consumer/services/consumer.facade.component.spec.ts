3f727d74d8d33d9e4c14fe15ba5ff94a4837d5fc
Kaitlyn Sookdhew <kaitlynsookdhew@gmail.com>
2023-07-27 20:12:28 +0200

    linting

--- app/src/app/features/sign-up/consumer/services/consumer.facade.component.spec.ts
+++ app/src/app/features/sign-up/consumer/services/consumer.facade.component.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { TestBed } from '@angular/core/testing';
 import { ConsumerFacade } from './consumer.facade';
 import { AuthFacade } from '@features/auth/services/auth.facade';
