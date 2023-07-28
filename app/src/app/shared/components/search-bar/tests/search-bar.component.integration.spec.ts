1cd56d7ebfb6f34dd6184b9dde6050ae87e74bad
Kaitlyn Sookdhew <kaitlynsookdhew@gmail.com>
2023-07-28 10:17:11 +0200

    fixed linting issue

--- app/src/app/shared/components/search-bar/tests/search-bar.component.integration.spec.ts
+++ app/src/app/shared/components/search-bar/tests/search-bar.component.integration.spec.ts
@@ -1,3 +1,4 @@
+ /* eslint-disable @typescript-eslint/naming-convention */
 //search bar integration test
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
