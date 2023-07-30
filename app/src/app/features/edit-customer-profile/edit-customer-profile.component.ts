762a20e660fae405f52586ca1636db8c8adc2db9
Gerard Nagura <u19004232@tuks.co.za>
2023-07-30 23:53:37 +0200

    integration

--- app/src/app/features/edit-customer-profile/edit-customer-profile.component.ts
+++ app/src/app/features/edit-customer-profile/edit-customer-profile.component.ts
@@ -4,43 +4,46 @@ import { ISellerProfile } from '../profile/models/seller-profile.interface';
 import { IConsumerProfile } from '../profile/models/consumer-profile.interface';
 import { Observable } from 'rxjs';
 import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-customer-profile',
   templateUrl: './edit-customer-profile.component.html',
-  styleUrls: ['./edit-customer-profile.component.scss']
+  styleUrls: ['./edit-customer-profile.component.scss'],
 })
 export class EditCustomerProfileComponent implements OnInit {
-
   customerprofileForm: FormGroup;
- // editProfileForm: FormGroup;
+  // editProfileForm: FormGroup;
   profile$: Observable<ISellerProfile | IConsumerProfile | null>;
 
-  constructor(public profileFacade: ProfileFacade, private formBuilder: FormBuilder) { 
+  constructor(
+    public profileFacade: ProfileFacade,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
     this.customerprofileForm = new FormGroup({
       username: new FormControl(),
-      email: new FormControl(), 
     });
 
-    this.profile$ = this.profileFacade.getProfile(); 
+    this.profile$ = this.profileFacade.getProfile();
   }
 
   ngOnInit() {
-    //this.profile$ = this.profileFacade.getProfile(); 
+    //this.profile$ = this.profileFacade.getProfile();
 
     this.profile$.subscribe(profile => {
       if (profile) {
         this.customerprofileForm.patchValue({
           username: profile.username,
-          email: profile.email,
         });
       }
     });
   }
 
   onSubmit() {
-    console.log("form submited")
+    this.profileFacade.updateProfile({
+      username: this.customerprofileForm.value.username,
+    });
+    this.router.navigate(['/customer-profile']);
   }
-
 }
