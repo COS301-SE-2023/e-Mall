579d69675dd93e7feb1b8be7a91fa530707146d4
Kaitlyn Sookdhew <kaitlynsookdhew@gmail.com>
2023-07-31 18:38:09 +0200

    update

--- app/cypress/e2e/home-to-seller.cy.ts
+++ app/cypress/e2e/home-to-seller.cy.ts
@@ -6,7 +6,7 @@ describe('template spec', () => {
     cy.contains('Merchants').click({ force: true });
     cy.wait(2000);
     cy.contains('Amazon').click({ force: true });
-    cy.url().should('include', '/seller-details?seller_id=36cc45f7-82ce-45b5-b56d-98683c0e06bf');
+    cy.url().should('include', '/seller-details');
 
   })
 })
