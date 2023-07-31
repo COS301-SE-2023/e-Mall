ff9c7637ac17103dc24040fa16124b52c3f83b27
Kaitlyn Sookdhew <kaitlynsookdhew@gmail.com>
2023-07-31 20:01:02 +0200

    added tests

--- app/src/app/features/profile/tests/profile.facade.spec.ts
+++ app/src/app/features/profile/tests/profile.facade.spec.ts
@@ -13,6 +13,8 @@ import { IError } from '@features/error/models/error.interface';
 import { IUser } from '@features/auth/models/user.interface';
 import { NgxsDispatchPluginModule } from '@ngxs-labs/dispatch-decorator';
 import { ProfileModule } from '../profile.module';
+import { Navigate } from '@ngxs/router-plugin';
+
 
 describe('ProfileFacade', () => {
   let facade: ProfileFacade;
@@ -220,4 +222,155 @@ describe('ProfileFacade', () => {
       new ProfileActions.ClearProfile()
     );
   });
+
+  /*it('should check wishlist and return true if item exists in wishlist', async () => {
+    const mockWishlist: number[] = [1, 2, 3];
+    store.reset({
+      profile: {
+        wishlist: mockWishlist,
+      },
+    });
+
+    const idToCheck = 2;
+    const result = await firstValueFrom(facade.checkWishlist(idToCheck));
+    expect(result).toBeTrue();
+  });
+*/
+  it('should check wishlist and return false if item does not exist in wishlist', async () => {
+    const mockWishlist: number[] = [1, 2, 3];
+    store.reset({
+      profile: {
+        wishlist: mockWishlist,
+      },
+    });
+
+    const idToCheck = 4;
+    const result = await firstValueFrom(facade.checkWishlist(idToCheck));
+    expect(result).toBeFalse();
+  });
+
+ /* it('should check followed sellers and return true if seller is followed', async () => {
+    const mockFollowedSellers: string[] = ['Seller1', 'Seller2'];
+    store.reset({
+      profile: {
+        followedSellers: mockFollowedSellers,
+      },
+    });
+
+    const sellerToCheck = 'Seller1';
+    const result = await firstValueFrom(
+      facade.checkFollowedSellers(sellerToCheck)
+    );
+    expect(result).toBeTrue();
+  });
+*/
+  it('should check followed sellers and return false if seller is not followed', async () => {
+    const mockFollowedSellers: string[] = ['Seller1', 'Seller2'];
+    store.reset({
+      profile: {
+        followedSellers: mockFollowedSellers,
+      },
+    });
+
+    const sellerToCheck = 'Seller3';
+    const result = await firstValueFrom(
+      facade.checkFollowedSellers(sellerToCheck)
+    );
+    expect(result).toBeFalse();
+  });
+
+ /* it('should toggle sellers and return Navigate action if user is not logged in', async () => {
+    spyOn(authFacade, 'isLoggedIn').and.returnValue(of(false));
+    const navigateSpy = spyOn(store, 'dispatch').and.returnValue(of(null));
+
+    const sellerToToggle = 'Seller1';
+    const result = await facade.toggleSellers(sellerToToggle);
+
+    expect(authFacade.isLoggedIn).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['sign-in']);
+    expect(result).toEqual(new Navigate(['sign-in']));
+  });
+
+  it('should toggle sellers and return Navigate action if user is a seller', async () => {
+    spyOn(authFacade, 'isLoggedIn').and.returnValue(of(true));
+    spyOn(authFacade, 'getUserType').and.returnValue(of('seller'));
+    const navigateSpy = spyOn(store, 'dispatch').and.returnValue(of(null));
+
+    const sellerToToggle = 'Seller1';
+    const result = await facade.toggleSellers(sellerToToggle);
+
+    expect(authFacade.isLoggedIn).toHaveBeenCalled();
+    expect(authFacade.getUserType).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['sales']);
+    expect(result).toEqual(new Navigate(['sales']));
+  });
+
+  it('should toggle sellers and return ToggleSellers action if user is a consumer', async () => {
+    spyOn(authFacade, 'isLoggedIn').and.returnValue(of(true));
+    spyOn(authFacade, 'getUserType').and.returnValue(of('consumer'));
+    const toggleFollowSpy = spyOn(profileService, 'toggleFollowSeller');
+    const dispatchSpy = spyOn(store, 'dispatch').and.returnValue(of(null));
+
+    const sellerToToggle = 'Seller1';
+    const result = await facade.toggleSellers(sellerToToggle);
+
+    expect(authFacade.isLoggedIn).toHaveBeenCalled();
+    expect(authFacade.getUserType).toHaveBeenCalled();
+    expect(toggleFollowSpy).toHaveBeenCalledWith(sellerToToggle);
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      new ProfileActions.ToggleSellers(sellerToToggle)
+    );
+    expect(result).toEqual(new ProfileActions.ToggleSellers(sellerToToggle));
+  });
+
+  it('should toggle wishlist and return Navigate action if user is not logged in', async () => {
+    spyOn(authFacade, 'isLoggedIn').and.returnValue(of(false));
+    const navigateSpy = spyOn(store, 'dispatch').and.returnValue(of(null));
+
+    const itemToToggle = 1;
+    const result = await facade.toggleWishlist(itemToToggle);
+
+    expect(authFacade.isLoggedIn).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['sign-in']);
+    expect(result).toEqual(new Navigate(['sign-in']));
+  });
+
+  it('should toggle wishlist and return Navigate action if user is a seller', async () => {
+    spyOn(authFacade, 'isLoggedIn').and.returnValue(of(true));
+    spyOn(authFacade, 'getUserType').and.returnValue(of('seller'));
+    const navigateSpy = spyOn(store, 'dispatch').and.returnValue(of(null));
+
+    const itemToToggle = 1;
+    const result = await facade.toggleWishlist(itemToToggle);
+
+    expect(authFacade.isLoggedIn).toHaveBeenCalled();
+    expect(authFacade.getUserType).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['sales']);
+    expect(result).toEqual(new Navigate(['sales']));
+  });
+
+  it('should toggle wishlist and return ToggleWishlist action if user is a consumer', async () => {
+    spyOn(authFacade, 'isLoggedIn').and.returnValue(of(true));
+    spyOn(authFacade, 'getUserType').and.returnValue(of('consumer'));
+    const toggleWishlistSpy = spyOn(profileService, 'toggleWishlist');
+    const dispatchSpy = spyOn(store, 'dispatch').and.returnValue(of(null));
+
+    const itemToToggle = 1;
+    const result = await facade.toggleWishlist(itemToToggle);
+
+    expect(authFacade.isLoggedIn).toHaveBeenCalled();
+    expect(authFacade.getUserType).toHaveBeenCalled();
+    expect(toggleWishlistSpy).toHaveBeenCalledWith(itemToToggle);
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      new ProfileActions.ToggleWishlist(itemToToggle)
+    );
+    expect(result).toEqual(new ProfileActions.ToggleWishlist(itemToToggle));
+  });
+*/
+  it('should call ngOnDestroy and unsubscribe authSubscription', () => {
+    const unsubscribeSpy = spyOn(facade['authSubscription'], 'unsubscribe');
+    facade.ngOnDestroy();
+    expect(unsubscribeSpy).toHaveBeenCalled();
+  });
+
 });
