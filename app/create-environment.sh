1c800c44be5019ac311c07cd589497c79c66c65f
J Yoon <u17260044@tuks.co.za>
2023-08-01 04:54:00 +0200

    changed script

---
+++ app/create-environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define environment variables
+PRODUCTION=$production
+USER_POOL_ID=$userPoolId
+USER_POOL_WEB_CLIENT_ID=$userPoolWebClientId
+API_URL=$apiUrl
+
+# Create environment file content
+ENVIRONMENT_FILE_CONTENT="export const environment = {
+  production: '$PRODUCTION',
+  cognito: {
+    userPoolId: '$USER_POOL_ID',
+    userPoolWebClientId: '$USER_POOL_WEB_CLIENT_ID',
+  },
+  apiUrl: '$API_URL',
+};"
+
+# Write environment file content to file
+echo "$ENVIRONMENT_FILE_CONTENT" > src/environments/environment.prod.ts
