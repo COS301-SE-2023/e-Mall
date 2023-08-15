91b365e4cee81e96a4200ee9a7e3f722ab7f9080
Letlhogonolo Rakgantsho <u21612812@tuks.co.za>
2023-08-16 00:14:31 +0200

    seller settings page UI

--- app/src/app/features/seller-dashboard-settings/seller-dashboard-settings.component.ts
+++ app/src/app/features/seller-dashboard-settings/seller-dashboard-settings.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileFacade } from '../profile/services/profile.facade';
+import { ISellerProfile } from '../profile/models/seller-profile.interface';
+import { IConsumerProfile } from '../profile/models/consumer-profile.interface';
+import { Observable } from 'rxjs';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-seller-dashboard-settings',
   templateUrl: './seller-dashboard-settings.component.html',
   styleUrls: ['./seller-dashboard-settings.component.scss']
 })
-export class SellerDashboardSettingsComponent {
+export class SellerDashboardSettingsComponent implements OnInit{
 
+  sellerProfileForm: FormGroup;
+  isUsernameChanged = false;
+  profile$: Observable<ISellerProfile | IConsumerProfile | null>;
+
+  constructor(
+    public profileFacade: ProfileFacade,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) 
+  {
+    this.sellerProfileForm = new FormGroup({
+      username: new FormControl(),
+      name: new FormControl(),
+    });
+
+    this.profile$ = this.profileFacade.getProfile();
+  }
+
+
+
+  ngOnInit() {
+    //this.profile$ = this.profileFacade.getProfile();
+
+    this.profile$.subscribe(profile => {
+      if (profile) {
+        this.sellerProfileForm.patchValue({
+          username: profile.username,
+        });
+      }
+    });
+  }
+
+  onSubmit() {
+    this.profileFacade.updateProfile({
+      username: this.sellerProfileForm.value.username,
+    });
+    this.router.navigate(['/customer-profile']);
+  }
+
+  onUsernameChange() {
+    
+    this.isUsernameChanged = true;
+  }
 }
--- app/src/app/features/seller-dashboard-settings/seller-dashboard-settings.module.ts
+++ app/src/app/features/seller-dashboard-settings/seller-dashboard-settings.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SellerDashboardSettingsComponent } from './seller-dashboard-settings.component';
+import { NavbarModule } from '@shared/components/navbar/navbar.module';
+import { FooterModule } from '@shared/components/footer/footer.module';
+import { IonicModule } from '@ionic/angular';
+import { SellerNavModule } from '@shared/components/seller-nav/seller-nav.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +14,12 @@ import { SellerDashboardSettingsComponent } from './seller-dashboard-settings.co
     SellerDashboardSettingsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    NavbarModule,
+    FooterModule,
+    SellerNavModule,
+    ReactiveFormsModule
   ]
 })
 export class SellerDashboardSettingsModule { }
