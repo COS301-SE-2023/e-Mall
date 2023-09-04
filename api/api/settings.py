19113a4c2fe46a62e43ff86272ff1290906902d8
J Yoon <u17260044@tuks.co.za>
2023-09-05 01:45:43 +0200

    try fix static files

--- api/api/settings.py
+++ api/api/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_ROOT = "/static/"
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
