0c9a4420eee6f713958cf3b2933550dd776512c8
J Yoon <u17260044@tuks.co.za>
2023-09-05 02:38:40 +0200

    z

--- api/api/settings.py
+++ api/api/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticc"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
