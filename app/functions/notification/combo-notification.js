6e68394df2d7d7a24592fe4f4f270a91ecaedb5e
Gerard Nagura <u19004232@tuks.co.za>
2023-09-08 14:00:45 +0200

    update combo notif

--- app/functions/notification/combo-notification.js
+++ app/functions/notification/combo-notification.js
@@ -1,55 +1,55 @@
 /* eslint-disable max-len */
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 exports.comboNotification = functions
-  .region('europe-west1')
-  .firestore.document('combos/{combo_id}/notifications/{notification_id}')
-  .onCreate((snap, context) => {
-    const data = snap.data();
-    const title = data.title;
-    const body = data.message;
-    const image = data.image;
-    const isRead = data.is_read;
-    const timestamp = data.timestamp;
-    const unixTimestamp = timestamp.toMillis();
+    .region("europe-west1")
+    .firestore.document("combos/{combo_id}/notifications/{notification_id}")
+    .onCreate((snap, context) => {
+      const data = snap.data();
+      const title = data.title;
+      const body = data.message;
+      const image = data.image;
+      const isRead = data.is_read;
+      const timestamp = data.timestamp;
+      const unixTimestamp = timestamp.toMillis();
 
-    // Get the list of users who should receive the combo notification
-    const comboRef = snap.ref.parent.parent;
-    return comboRef.get().then(comboDoc => {
-      const targetUsers = comboDoc.data().target_users;
+      // Get the list of users who should receive the combo notification
+      const comboRef = snap.ref.parent.parent;
+      return comboRef.get().then((comboDoc) => {
+        const targetUsers = comboDoc.data().target_users;
 
-      if (targetUsers && targetUsers.length > 0) {
+        if (targetUsers && targetUsers.length > 0) {
         // Get the device tokens of all target users with a single query
-        return admin
-          .firestore()
-          .collection('users')
-          .where(admin.firestore.FieldPath.documentId(), 'in', targetUsers)
-          .get()
-          .then(querySnapshot => {
-            const deviceTokens = querySnapshot.docs.map(
-              doc => doc.data().device_token
-            );
+          return admin
+              .firestore()
+              .collection("users")
+              .where(admin.firestore.FieldPath.documentId(), "in", targetUsers)
+              .get()
+              .then((querySnapshot) => {
+                const deviceTokens = querySnapshot.docs.map(
+                    (doc) => doc.data().device_token,
+                );
 
-            // Create the notification payload
-            const message = {
-              notification: {
-                title: title,
-                body: body,
-                image: image,
-              },
-              data: {
-                is_read: isRead.toString(),
-                timestamp: unixTimestamp.toString(),
-              },
-              tokens: deviceTokens,
-            };
+                // Create the notification payload
+                const message = {
+                  notification: {
+                    title: title,
+                    body: body,
+                    image: image,
+                  },
+                  data: {
+                    is_read: isRead.toString(),
+                    timestamp: unixTimestamp.toString(),
+                  },
+                  tokens: deviceTokens,
+                };
 
-            // Send the notification to all device tokens
-            return admin.messaging().sendMulticast(message);
-          });
-      } else {
-        return null;
-      }
+                // Send the notification to all device tokens
+                return admin.messaging().sendMulticast(message);
+              });
+        } else {
+          return null;
+        }
+      });
     });
-  });
