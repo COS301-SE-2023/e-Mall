9a1a73f291bdb2a2359dddcabbe59f54bd3fa673
Letlhogonolo Rakgantsho <u21612812@tuks.co.za>
2023-09-08 06:39:00 +0200

    product page loader

--- app/src/app/features/product/product.component.ts
+++ app/src/app/features/product/product.component.ts
@@ -33,6 +33,7 @@ export class ProductComponent implements OnInit, OnDestroy {
   currencyCode = 'ZAR';
 
   showSpinner = true;
+  showSpinner2 = true;
 
   // Your timer function
   
@@ -58,11 +59,13 @@ export class ProductComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     
     this.showSpinner = true;
+    this.showSpinner2 = true;
     
     setTimeout(() => {
       this.showSpinner = false;
+      this.showSpinner2 = false;
       
-    }, 4500); 
+    }, 6000); 
 
     this.paramMapSubscription = this.route.queryParamMap.subscribe(params => {
       this.selectedImage = '';
@@ -179,6 +182,13 @@ export class ProductComponent implements OnInit, OnDestroy {
     el.scrollIntoView();
   }
   onlyInStockToggler() {
+    this.showSpinner2 = true;
+    
+    setTimeout(() => {
+      this.showSpinner2 = false;
+      
+    }, 6000);
+
     this.divClicked = !this.divClicked;
 
     this.sellers$ = this.productService.getSellerList(
