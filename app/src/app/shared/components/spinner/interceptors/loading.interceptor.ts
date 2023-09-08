f6e34058e7cd03a44bb1ee4600d8344fa65bb488
Letlhogonolo Rakgantsho <u21612812@tuks.co.za>
2023-09-08 03:57:53 +0200

    loaders

--- app/src/app/shared/components/spinner/interceptors/loading.interceptor.ts
+++ app/src/app/shared/components/spinner/interceptors/loading.interceptor.ts
@@ -14,27 +14,26 @@ import { LoaderService } from '../services/loader.service';
 export class LoadingInterceptor implements HttpInterceptor {
 
   private totalRequests = 0;
+  private isIntercepted = false;
 
   constructor(private loadingService: LoaderService) {}
 
   
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    
-    if (this.totalRequests === 0) {
-      // If there are no active requests, show the loading spinner
-      this.loadingService.setLoading(true);
-      console.log('caught')
-    }
-    this.totalRequests++;
+    if (!this.isIntercepted) {
+      console.log('Intercepted');
+      this.isIntercepted = true; // Mark as intercepted
 
-    return next.handle(request).pipe(
-      finalize(() => {
-        this.totalRequests--;
-        if (this.totalRequests === 0) {
+      this.loadingService.setLoading(true);
+      
+      return next.handle(request).pipe(
+        finalize(() => {
           this.loadingService.setLoading(false);
-        }
-      })
-    );
+        })
+      );
+    }
+    // If already intercepted, pass the request without intercepting again.
+    return next.handle(request);
   }
 
 }
