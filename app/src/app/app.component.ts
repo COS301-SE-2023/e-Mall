fcd40842881ebe7c424c8735f32b547a800f8b5c
J Yoon <u17260044@tuks.co.za>
2023-09-10 03:57:26 +0200

    fixed menu not showing up in other pages

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotificationFacade } from './features/notification/services/notification.facade';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(public notificationFacade: NotificationFacade) {}
+  notificationMenuClosed() {
+    this.notificationFacade.isMenuOpen$.next(false);
+  }
 }
