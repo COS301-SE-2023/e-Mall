34ebf082e797e3f03b3987c48d5bc00743f7a118
Kaitlyn Sookdhew <kaitlynsookdhew@gmail.com>
2023-09-16 17:19:46 +0200

    Create product ui

--- app/src/app/features/inventory/components/popovernew/popovernew.component.ts
+++ app/src/app/features/inventory/components/popovernew/popovernew.component.ts
@@ -10,48 +10,97 @@ import { InventoryService } from '../../servicies/inventory.service';
   styleUrls: ['./popovernew.component.scss'],
 })
 export class PopovernewComponent implements OnInit {
-    
   newClicked = false;
   selectForm!: FormGroup;
-  nextClicked=false;
+  nextClicked = false;
   similarProducts!: any;
-  nameForm!:FormGroup;
-  
-  newForm!: FormGroup;
-    constructor(
-        private popoverController: PopoverController,
-        private inventoryFacade: InventoryFacade,
-        private modalController: ModalController,
-        private fb: FormBuilder,
-        private inventoryService: InventoryService
-      ) {}
-    ngOnInit(): void {
-        console.log("New product")
-        this.nameForm=this.fb.group({
-          newName:['',Validators.required],
-        });
-    }
-    closeModal() {
-        this.modalController.dismiss();
-      }
+  nameForm!: FormGroup;
+  isSelected = false;
+  productForm!: FormGroup;
+  productSellerForm!:FormGroup;
 
-      newClick() {
-        this.newClicked = true;
-      }
-      nextClick(){
-        if(this.nameForm.valid){
-          const newname=this.nameForm.value.newName;
-          console.log(newname)
-        const data={
-          name: newname
-        }
-        console.log(data)
-        this.similarProducts = this.inventoryService.getSimilarProducts(data);
-        console.log(this.similarProducts)
-        this.nextClicked=true;
-      }
+  constructor(
+    private popoverController: PopoverController,
+    private inventoryFacade: InventoryFacade,
+    private modalController: ModalController,
+    private fb: FormBuilder,
+    private inventoryService: InventoryService
+  ) {}
+  ngOnInit(): void {
+    console.log('New product');
+    this.nameForm = this.fb.group({
+      newName: ['', Validators.required],
+    });
+  }
+  closeModal() {
+    this.modalController.dismiss();
+  }
+
+  newClick() {
+    this.newClicked = true;
+    this.nextClicked = false;
+    this.isSelected = false;
+  }
+  nextClick() {
+    if (this.nameForm.valid) {
+      const newname = this.nameForm.value.newName;
+      const data = {
+        name: newname,
+      };
+      this.similarProducts = this.inventoryService.getSimilarProducts(data);
+      console.log(this.similarProducts);
+      this.nextClicked = true;
     }
-      Done(){
-        console.log("Done")
-      }
+  }
+
+  selectProd() {
+    this.isSelected = true;
+    this.newClicked = false;
+    this.nextClicked = false;
+  }
+
+  backClick() {
+    this.nextClicked = false;
+    this.isSelected = false;
+    this.newClicked = false;
+  }
+  NewProduct(){
+    console
+  }
+
+  formatNumber(e: any, val: string) {
+    const separator = '.';
+    const decimais = 2;
+    if (!e.detail.value) return '';
+
+    let [num, decimal] = e.detail.value.split(separator);
+    console.log(num, decimal);
+
+    num = num.toLocaleString();
+
+    if (decimal) {
+      decimal = decimal.toString().padEnd(decimais, '0');
+    } else {
+      decimal = '00';
+    }
+    const ret = num + '.' + decimal;
+    if (val === 'price') {
+      return this.setFormControlValue('price', ret);
+    } else if (val === 'discount') {
+      return this.setFormControlValue('discount', ret);
+    }
+  }
+
+  getFormControl(field: string) {
+    return this.productForm.get(field);
+  }
+  setFormControlValue(field: string, value: any) {
+    this.productForm.get(field)?.setValue(value);
+  }
+  getFormControlValue(field: string) {
+    return this.productForm.get(field)?.value;
+  }
+  Done() {
+    console.log('Done');
+  }
 }
