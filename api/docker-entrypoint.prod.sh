b4180fd3628fa0fb47cb328ac5c008d12f62e597
J Yoon <u17260044@tuks.co.za>
2023-09-17 22:07:40 +0200

    z

--- api/docker-entrypoint.prod.sh
+++ api/docker-entrypoint.prod.sh
@@ -7,6 +7,7 @@ python manage.py collectstatic --no-input
 gunicorn api.wsgi:application --bind 0.0.0.0:3000 --daemon
 
 nginx -g 'daemon off;'
+#run celery
 celery -A api.celery worker --loglevel=info &
 
 exec "$@"
