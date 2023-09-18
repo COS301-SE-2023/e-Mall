f7296bcbc1b87a557810650a1afe992c9965fcce
J Yoon <u17260044@tuks.co.za>
2023-09-18 02:17:59 +0200

    populate scripts

--- api/docker-entrypoint.prod.sh
+++ api/docker-entrypoint.prod.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo "Reset the database"
+python manage.py reset_db --noinput
+
+echo "Flush the database record"
+python manage.py flush --no-input
 
 python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+
+bash /command/populate_all.sh
 
 gunicorn api.wsgi:application --bind 0.0.0.0:3000 --daemon
 
