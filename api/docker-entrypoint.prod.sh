abce292fc5743a1a32cdc6f46e1501a2a5c12a7e
J Yoon <u17260044@tuks.co.za>
2023-09-18 13:01:57 +0200

    celery command move

--- api/docker-entrypoint.prod.sh
+++ api/docker-entrypoint.prod.sh
@@ -4,12 +4,13 @@ python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-bash command/populate_all.sh
+# bash command/populate_all.sh
+
+#run celery
+celery -A api.celery worker --loglevel=info &
 
 gunicorn api.wsgi:application --bind 0.0.0.0:3000 --daemon
 
 nginx -g 'daemon off;'
-#run celery
-celery -A api.celery worker --loglevel=info &
 
 exec "$@"
