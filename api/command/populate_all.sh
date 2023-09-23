acc1ce2dbb0d7eebc82b3a0e1a4249e3dd2b9193
MaverickGDN03 <u19004232@tuks.co.za>
2023-09-23 19:17:04 +0200

    fixed create product issue

--- api/command/populate_all.sh
+++ api/command/populate_all.sh
@@ -4,4 +4,4 @@ python manage.py populateproducts
 python manage.py populatesellers
 python manage.py populateproductsellers
 python manage.py populateconsumers
-python manage.py populateanalytics
+# python manage.py populateanalytics
