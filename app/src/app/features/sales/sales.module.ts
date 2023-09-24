84e96d4828a2e8c2262434fc132f420d41a840f2
J Yoon <u17260044@tuks.co.za>
2023-09-24 03:49:35 +0200

    loader for sales page done

--- app/src/app/features/sales/sales.module.ts
+++ app/src/app/features/sales/sales.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SalesRoutingModule } from './sales-routing.module';
-import { SalesComponent } from './sales.component';
+import SalesComponent from './sales.component';
 import { IonicModule } from '@ionic/angular';
 
 import { SellerNavModule } from '@shared/components/seller-nav/seller-nav.module';
 
 @NgModule({
   declarations: [SalesComponent],
-  imports: [CommonModule, SalesRoutingModule, IonicModule,SellerNavModule],
+  imports: [CommonModule, SalesRoutingModule, IonicModule, SellerNavModule],
   exports: [SalesComponent],
 })
 export class SalesModule {}
