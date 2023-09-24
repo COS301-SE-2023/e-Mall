54a12ded2b6f7d805f38d85f41ca3ce48949d42f
J Yoon <u17260044@tuks.co.za>
2023-09-25 01:43:03 +0200

    uncommented that settings

--- api/api/utils.py
+++ api/api/utils.py
@@ -18,50 +18,49 @@ s3 = boto3.client(
 
 
 def upload_to_spaces(url, folder_name, acl="public-read"):
-    # if not settings.DEBUG:
-    try:
-        response = requests.get(url)
-        file = BytesIO(response.content)
+    if not settings.DEBUG:
+        try:
+            response = requests.get(url)
+            file = BytesIO(response.content)
 
-        # print("********", url)
+            # print("********", url)
 
-        query_params = urlparse(url).query
-        # Extract the image URL from the query parameters, if present
-        image_url = parse_qs(query_params).get("url", [None])[0]
-        if image_url:
-            # If the image URL is present, decode it
-            image_url = unquote(image_url)
-        else:
-            # If the image URL is not present, use the original URL
-            image_url = url
-        # Get the filename from the image URL
-        filename = os.path.basename(urlparse(image_url).path)
-        # If the filename has a query string or fragment identifier, remove it
-        filename = filename.split("?")[0].split("#")[0]
-        # print(filename)
+            query_params = urlparse(url).query
+            # Extract the image URL from the query parameters, if present
+            image_url = parse_qs(query_params).get("url", [None])[0]
+            if image_url:
+                # If the image URL is present, decode it
+                image_url = unquote(image_url)
+            else:
+                # If the image URL is not present, use the original URL
+                image_url = url
+            # Get the filename from the image URL
+            filename = os.path.basename(urlparse(image_url).path)
+            # If the filename has a query string or fragment identifier, remove it
+            filename = filename.split("?")[0].split("#")[0]
+            # print(filename)
 
-        # Get the file extension
-        _, ext = os.path.splitext(filename)
-        # Generate a random unique file name with the same extension
-        filename = f"{uuid.uuid4()}{ext}"
-        # print("###", filename)
-        s3.upload_fileobj(
-            file,
-            folder_name,
-            filename,
-            ExtraArgs={
-                "ACL": acl,
-            },
-        )
+            # Get the file extension
+            _, ext = os.path.splitext(filename)
+            # Generate a random unique file name with the same extension
+            filename = f"{uuid.uuid4()}{ext}"
+            # print("###", filename)
+            s3.upload_fileobj(
+                file,
+                folder_name,
+                filename,
+                ExtraArgs={
+                    "ACL": acl,
+                },
+            )
 
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
-    return f"https://bucket.emall.space/{folder_name}/{filename}"
+        except FileNotFoundError:
+            print("The file was not found")
+            return False
+        except NoCredentialsError:
+            print("Credentials not available")
+            return False
+        return f"https://bucket.emall.space/{folder_name}/{filename}"
 
-
-# else:
-#     return url
+    else:
+        return url
