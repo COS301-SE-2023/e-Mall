0d898bc163eca82166361e8b89b9302134310503
J Yoon <u17260044@tuks.co.za>
2023-10-26 02:21:11 +0200

    removed lint issues

--- app/src/app/shared/components/navbar/search-modal/search-modal.component.ts
+++ app/src/app/shared/components/navbar/search-modal/search-modal.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import { IonModal } from '@ionic/angular';
-import { SearchBarComponent } from '../../search-bar/search-bar.component';
 
 @Component({
-    selector: 'app-search-modal',
-    templateUrl: './search-modal.component.html',
-    styleUrls: ['./search-modal.component.scss'],
-  })
+  selector: 'app-search-modal',
+  templateUrl: './search-modal.component.html',
+  styleUrls: ['./search-modal.component.scss'],
+})
 export class SearchModalComponent {
-  constructor(private modalController: ModalController, private router: Router) {}
+  constructor(private modalController: ModalController) {}
 
   closeMenuModal() {
     this.modalController.dismiss();
   }
-
-  
 }
